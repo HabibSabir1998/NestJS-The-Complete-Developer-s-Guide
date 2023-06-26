191fca4530f7cdecd36e763f29b87018d99c94e5
Habib Sabir <habib.kasbati@gmail.com>
2023-06-26 17:51:11 +0500

    create repo and services

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,12 +1,24 @@
+import { MessagesService } from './messages.service';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
   @Post()
-  createMessage(@Body() body: CreateMessageDto) {}
-  @Get('/:id')
-  getMessage(@Param('id') id: string) {}
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+  @Get('/:id') 
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
 }
---
+++ messages/src/messages/messages.service.ts
@@ -0,0 +1,20 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  messagesRepo: MessagesRepository;
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(message: string) {
+    return this.messagesRepo.create(message);
+  }
+}
