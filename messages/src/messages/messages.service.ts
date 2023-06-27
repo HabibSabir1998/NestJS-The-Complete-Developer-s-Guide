ba97614f7e70d61e7f926b5dd1130c0ccb955584
Habib Sabir <habib.kasbati@gmail.com>
2023-06-27 14:26:02 +0500

    feat: section 5 completed

--- messages/src/messages/messages.service.ts
+++ messages/src/messages/messages.service.ts
@@ -1,5 +1,6 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
-
+@Injectable()
 export class MessagesService {
   constructor(public messagesRepo: MessagesRepository) {}
 
